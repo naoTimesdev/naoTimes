333daac4bb28774b62f8e290cdc427eccd0b3f7f
noaione <noaione0809@gmail.com>
2020-03-04 22:35:21 +0700

    webscript/naoTimes.js: update file to use new API. Masih hardcode url ke s.ihateani.me

--- webscript/naoTimes.js
+++ webscript/naoTimes.js
@@ -1,7 +1,3 @@
-// Change this //
-var gist_raw_url = 'MASUKAN URL RAW GIST TANPA REVISI COMMIT';
-/////////////////
-
 function nT_resolve_status(response) {
 	if (response.status >= 200 && response.status < 300) {
 		return Promise.resolve(response)
@@ -16,7 +12,7 @@ function nT_json_data(response) {
 
 function naoTimesProcess(disID) {
 	console.log('Fetching naoTimes data');
-	fetch(gist_raw_url)
+	fetch(`https://s.ihateani.me/api/v2/utang/${disID}?pretty=1`)
 	.then(nT_resolve_status)
 	.then(nT_json_data)
 	.then(function(nT_data) {
@@ -48,60 +44,48 @@ function naoTimesProcess(disID) {
 		};
 		var div_data = document.getElementById("naotimes");
 		var loading_elem = document.getElementById('naotimes-loading');
-		var json_data = JSON.parse(nT_data)
-		console.log('Parsing naoTimes data');
-		var dis_data = json_data[disID];
-		var available_anime = [];
-		var word_replace = {"ENC": "Encode", "ED": "Edit", "TM": "Timing"};
-
-		for (a in dis_data['anime']) {
-			if (a == "alias") {continue};
-			available_anime.push(a);
+		var json_data = JSON.parse(nT_data);
+		if (json_data.length == 0) {
+			var h2_node = document.createElement("h2");
+			h2_node.classList.add("naotimes-animetitle")
+			var h2_textNode = document.createTextNode("Tidak ada data utang");
+			h2_node.appendChild(h2_textNode);
+			loading_elem.parentNode.removeChild(loading_elem);
+			return 0;
 		}
-
-		for (ava in available_anime) {
-			var textRes = [];
-			var current_episode = '';
-			var status_list = dis_data['anime'][available_anime[ava]]['status'];
-			for (stat in status_list) {
-				if (status_list[stat]['status'] != 'released') {
-					current_episode += stat;
-					var ep_status = status_list[stat]['staff_status'];
-					for (key in ep_status) {
-						if (ep_status[key] == 'x') {
-							textRes.push(key);
-						}
-					}
-					break;
-				} else {
-					continue;
+		console.log('Parsing naoTimes data');
+		json_data.forEach(function (item, index) {
+			var statuses = item['status'];
+			var current_stat = [];
+			var current_episode = item['episode'];
+			for (stat in statuses) {
+				if (!statuses[stat]) {
+					current_stat.push(stat);
 				}
 			}
-			if (Array.isArray(textRes) && textRes.length) {
-				textRes = textRes.join(" ");
-				for (word in word_replace) {
-					textRes = textRes.replace(word, word_replace[word]);
-				}
-				get_time = formatDate(status_list[current_episode]['airing_time']);
-				if (get_time != false) {
-					textRes = get_time;
+
+			if (Array.isArray(current_stat) && current_stat.length) {
+				current_stat = current_stat.join(" ");
+				get_time = formatDate(item['airing_time']);
+				if (get_time) {
+					current_stat = get_time;
 				}
 				var h2_node = document.createElement("h2");
 				h2_node.classList.add("naotimes-animetitle")
-				var h2_textNode = document.createTextNode(available_anime[ava]);
+				var h2_textNode = document.createTextNode(item['title']);
 				var stat_node = document.createElement("ul");
 				stat_node.classList.add("naotimes-animeprogress")
 				if (current_episode.length < 2) { // pad number
 					var current_episode = '0' + current_episode;
 				}
-				var final_text = current_episode + ' @ ' + textRes;
+				var final_text = current_episode + ' @ ' + current_stat;
 				var stat_textNode = document.createTextNode(final_text);
 				h2_node.appendChild(h2_textNode);
 				stat_node.appendChild(stat_textNode);
 				div_data.appendChild(h2_node);
 				div_data.appendChild(stat_node);
 			}
-		}
+		})
 		loading_elem.parentNode.removeChild(loading_elem);
 		console.log('Finished!')
 	}).catch(function(error) {
