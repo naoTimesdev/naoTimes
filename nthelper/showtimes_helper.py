c44e4488244064855ddd7f22bcd82635428a041a
NoAiOne <aimanmh@my.smccd.edu>
2020-06-03 10:29:04 +0700

    showtimes: solve a race-condition that might happen

--- nthelper/showtimes_helper.py
+++ nthelper/showtimes_helper.py
@@ -1,7 +1,44 @@
+import asyncio
+import functools
 from copy import deepcopy
 
 import motor.motor_asyncio
 import pymongo
+
+
+def safe_asynclock(func):
+    """A thread-safe/async-safe decorator lock mechanism to fight race-condition
+    Should be safe enough™
+
+    :param func: Function to wraps
+    :type func: Callable
+    :return: A async wrapped function with lock mechanism on try/catch block.
+             Function put into try/catch block just in case the function fail horribly
+    :rtype: Callable
+    """
+    @functools.wraps(func)
+    async def async_wrapper(self, *args, **kwargs):
+        try:
+            print("[SAFELOCK] Acquiring lock...")
+            await self._acquire_lock()
+            print("[SAFELOCK] Running function...")
+            ret = await func(self, *args, **kwargs)
+            print("[SAFELOCK] Releasing lock...")
+            await self._release_lock()
+            return ret
+        except:
+            print("[SAFELOCK] Exception occured, releasing lock...")
+            await self._release_lock()
+            ret = None
+            if args:
+                if len(args) == 1:
+                    ret = None, None
+            if kwargs:
+                if kwargs.get("collection"):
+                    ret = None, None
+            return ret
+    return async_wrapper
+
 
 
 class naoTimesDB:
@@ -16,6 +53,64 @@ class naoTimesDB:
         self.clientsync = pymongo.MongoClient("mongodb://{}:{}".format(ip_hostname, port))
         self.dbsync = self.clientsync[dbname]
         self.srv_re = {"name": {"$regex": r"^srv"}}
+        self.__locked = False
+
+    @property
+    def is_locked(self):
+        return self.__locked
+
+    async def _acquire_lock(self):
+        while True:
+            if not self.__locked:
+                break
+            await asyncio.sleep(0.5)
+        self.__locked = True
+
+    async def _release_lock(self):
+        self.__locked = False
+
+    @safe_asynclock
+    async def fetch_data(self, collection: str) -> tuple:
+        coll = self.db[collection]
+        coll_cur = coll.find({})
+        res = list(await coll_cur.to_list(length=100))
+        res = res[0]
+        del res['_id']
+        return res, collection
+
+    @safe_asynclock
+    async def update_data(self, coll_key: str, data: dict) -> bool:
+        upd = {"$set": data}
+        print('\t[ntDB] Updating collection: {}'.format(coll_key))
+        coll = self.db[coll_key]
+        res = await coll.update_one({}, upd)
+        if res.acknowledged:
+            print('\t[ntDB] INFO: {} data updated.'.format(coll_key))
+            return True
+        print('\t[ntDB] ERROR: Failed to update {} data.'.format(coll_key))
+        return False
+
+    @safe_asynclock
+    async def insert_new(self, coll_key: str, data: dict) -> bool:
+        coll = self.db[coll_key]
+        print('\t[ntDB] INFO: Adding new data: {}'.format(coll_key))
+        result = await coll.insert_one(data)
+        if result.acknowledged:
+            self.logger.info(f"\tInserted with IDs: {result.inserted_id}")
+            return True
+        self.logger.error("\tFailed to insert new data.")
+        return False
+
+    @safe_asynclock
+    async def insert_new_sync(self, coll_key: str, data: dict) -> bool:
+        srv = self.dbsync[coll_key]
+        print('\t[ntDB] INFO: Adding new data: {}'.format(coll_key))
+        res = srv.insert(data, check_keys=False)
+        if res:
+            print('\t[ntDB] INFO: {} data inserted.'.format(coll_key))
+            return True
+        print('\t[ntDB] ERROR: Failed to insert {} data.'.format(coll_key))
+        return False
 
     async def _precheck_server_name(self, namae):
         if not isinstance(namae, str):
@@ -40,14 +135,12 @@ class naoTimesDB:
         admin_list = await self.get_top_admin()
         json_data['supermod'] = admin_list
         server_collection = await self.db.list_collection_names(filter=self.srv_re)
-        for collection in server_collection:
-            print('[ntDB] INFO2: Fetching server: {}'.format(collection))
-            srv = self.db[collection]
-            srv_cur = srv.find({})
-            srv = list(await srv_cur.to_list(length=100))
-            srv = srv[0]
-            del srv['_id']
-            json_data[collection[4:]] = srv
+        print("[ntDB] INFO: Creating tasks...")
+        server_tasks = [self.fetch_data(coll) for coll in server_collection]
+        for srv_task in asyncio.as_completed(server_tasks):
+            srv, name = await srv_task
+            print('[ntDB] INFO2: Fetching server: {}'.format(name))
+            json_data[name[4:]] = srv
         print('[ntDB] INFO: Dumping data...')
         return json_data
 
@@ -61,45 +154,32 @@ class naoTimesDB:
             print('[ntDB] Redoing collection: {}'.format(ksrv))
             ksrv = await self._precheck_server_name(ksrv)
             if ksrv in server_collection:
-                upd_data = {
-                    "$set": dataset[ksrv[4:]]
-                }
                 while True:
                     print('\t[ntDB] Updating collection: {}'.format(ksrv))
-                    srv = self.db[ksrv]
-                    res = await srv.update_one({}, upd_data)
+                    res = await self.update_data(ksrv, dataset[ksrv[4:]])
                     if res:
                         print('\t[ntDB] INFO: Updated.')
                         break
             else:
                 while True:
-                    srv = self.dbsync[ksrv]
                     print('\t[ntDB] INFO: Adding new data: {}'.format(ksrv))
-                    res = srv.insert(dataset[ksrv[4:]], check_keys=False)
+                    res = await self.insert_new_sync(ksrv, dataset[ksrv[4:]])
                     if res:
                         break
 
-        upd_adm = {
-            "$set": {
-                "server_admin": dataset['supermod']
-            }
-        }
         print('[ntDB] INFO: Updating top admin...')
         while True:
-            admin = self.db['server_admin']
-            res = await admin.update_one({}, upd_adm)
-            if res.acknowledged:
+            res = await self.update_data("server_admin", {"server_admin": dataset["supermod"]})
+            if res:
                 print('[ntDB] INFO: Updated.')
                 break
-        print('[ntDB] IFNO: Succes patching database')
+        print('[ntDB] INFO: Success patching database')
         return True
 
     async def get_top_admin(self):
         print('[ntDB] INFO: Fetching top admin/server admin.')
-        admin = self.db['server_admin']
-        admin = admin.find({})
-        admin = list(await admin.to_list(length=100))
-        admin = admin[0]['server_admin']
+        admin, _ = await self.fetch_data("server_admin")
+        admin = admin['server_admin']
         return admin
 
     async def add_top_admin(self, adm_id):
@@ -108,17 +188,9 @@ class naoTimesDB:
         adm_id = str(adm_id)
         if adm_id in adm_list:
             return True, "Sudah ada"
-        upd = {
-            "$set": {
-                "server_admin": adm_list
-            }
-        }
-        admin = self.db['server_admin']
-        res = await admin.update_one({}, upd)
-        if res.acknowledged:
-            print('[ntDB] INFO: Top admin data updated.')
+        res = await self.update_data("server_admin", {"server_admin": adm_list})
+        if res:
             return True, 'Updated'
-        print('[ntDB] ERROR: Failed to update top admin data.')
         return False, 'Gagal menambah top admin baru.'
 
     async def remove_top_admin(self, adm_id):
@@ -127,19 +199,12 @@ class naoTimesDB:
         adm_id = str(adm_id)
         if adm_id in adm_list:
             adm_list.remove(adm_id)
-        upd = {
-            "$set": {
-                "server_admin": adm_list
-            }
-        }
-        admin = self.db['server_admin']
-        res = await admin.update_one({}, upd)
-        if res.acknowledged:
-            print('[ntDB] INFO: Top admin data updated.')
+        res = await self.update_data("server_admin", {"server_admin": adm_list})
+        if res:
             return True, 'Updated'
-        print('[ntDB] ERROR: Failed to update top admin data.')
-        return False, 'Gagal menambah top admin baru.'
+        return False, 'Gagal menghapus top admin.'
 
+    @safe_asynclock
     async def get_server_list(self):
         print('[ntDB] INFO: Fetching server list')
         srv_list = await self.db.list_collection_names(filter=self.srv_re)
@@ -149,35 +214,26 @@ class naoTimesDB:
     async def get_server(self, server):
         server = await self._precheck_server_name(server)
         print('[ntDB] INFO: Fetching server set: {}'.format(server))
-        srv_list = await self.db.list_collection_names(filter=self.srv_re)
+        srv_list = await self.get_server_list()
         if server not in srv_list:
             print('[ntDB] WARN: cannot found server in database.')
             return {}
 
-        srv = self.db[server]
-        srv_cur = srv.find({})
-        srv = list(await srv_cur.to_list(length=100))
-        srv = srv[0]
-        del srv['_id']
+        srv, _ = await self.fetch_data(server)
         return srv
 
-    async def update_data(self, server, dataset):
-        dataset = {
-            "$set": dataset
-        }
+    async def update_data_server(self, server, dataset):
         server = await self._precheck_server_name(server)
         print('[ntDB] INFO: Updating data for server: {}'.format(server))
-        srv_list = await self.db.list_collection_names(filter=self.srv_re)
+        srv_list = await self.get_server_list()
         if server not in srv_list:
             print('[ntDB] WARN: cannot found server in database.')
             return False, 'Server tidak terdaftar di database, gunakan metode `new_server`'
 
         srv = self.db[server]
-        res = await srv.update_one({}, dataset)
-        if res.acknowledged:
-            print('[ntDB] INFO: Server data updated.')
+        res = await self.update_data(server, dataset)
+        if res:
             return True, 'Updated'
-        print('[ntDB] ERROR: Failed to update server data.')
         return False, 'Gagal mengupdate server data.'
 
     async def add_admin(self, server, adm_id):
@@ -191,7 +247,7 @@ class naoTimesDB:
         if adm_id not in srv_data['serverowner']:
             srv_data['serverowner'].append(adm_id)
 
-        res, msg = await self.update_data(server, srv_data)
+        res, msg = await self.update_data_server(server, srv_data)
         return res, msg
 
     async def remove_admin(self, server, adm_id):
@@ -205,16 +261,16 @@ class naoTimesDB:
         if adm_id in srv_data['serverowner']:
             srv_data['serverowner'].remove(adm_id)
 
-        res, msg = await self.update_data(server, srv_data)
+        res, msg = await self.update_data_server(server, srv_data)
         return res, msg
 
     async def new_server(self, server, admin_id, announce_channel = None):
         server = await self._precheck_server_name(server)
         print('[ntDB] INFO: Adding data for a new server: {}'.format(server))
-        srv_list = await self.db.list_collection_names(filter=self.srv_re)
+        srv_list = await self.get_server_list()
         if server in srv_list:
-            print('[ntDB] WARN: found server in database, please use `update_data` method')
-            return False, 'Server terdaftar di database, gunakan metode `update_data`'
+            print('[ntDB] WARN: found server in database, please use `update_data_server` method')
+            return False, 'Server terdaftar di database, gunakan metode `update_data_server`'
 
         dataset = {
             "serverowner": [str(admin_id)],
@@ -227,8 +283,7 @@ class naoTimesDB:
         if announce_channel:
             dataset['announce_channel'] = announce_channel
 
-        srv = self.dbsync[server]
-        res = srv.insert(dataset, check_keys=False)
+        res = await self.insert_new_sync(server, dataset)
         if res:
             res, msg = await self.add_top_admin(str(admin_id))
             print('[ntDB] INFO: Server data updated.')
@@ -239,7 +294,7 @@ class naoTimesDB:
     async def remove_server(self, server, admin_id):
         server = await self._precheck_server_name(server)
         print('[ntDB] INFO: Expunging data for a server: {}'.format(server))
-        srv_list = await self.db.list_collection_names(filter=self.srv_re)
+        srv_list = await self.get_server_list()
         if server not in srv_list:
             print('[ntDB] WARN: Cannot find server in database, ignoring...')
             return True
@@ -263,7 +318,7 @@ class naoTimesDB:
             return False, 'Server tidak terdaftar di naoTimes.'
         srv_data['konfirmasi'][confirm_id] = target_data
 
-        res, msg = await self.update_data(target_server, srv_data)
+        res, msg = await self.update_data_server(target_server, srv_data)
         return res, msg
 
     async def kolaborasi_konfirmasi(self, source_server, target_server, srv1_data, srv2_data):
@@ -280,10 +335,10 @@ class naoTimesDB:
         if not source_srv_data:
             return False, 'Server awal tidak terdaftar di naoTimes.'
 
-        res, msg = await self.update_data(source_server, srv1_data)
+        res, msg = await self.update_data_server(source_server, srv1_data)
         print('[ntDB] INFO: Acknowledged? {}'.format(res))
         print('[ntDB] Message: {}'.format(msg))
-        res, msg = await self.update_data(target_server, srv2_data)
+        res, msg = await self.update_data_server(target_server, srv2_data)
         print('[ntDB] INFO: Acknowledged? {}'.format(res))
         print('[ntDB] Message: {}'.format(msg))
         return res, msg
@@ -297,7 +352,7 @@ class naoTimesDB:
 
         if confirm_id in srv_data['kolaborasi']:
             del srv_data['kolaborasi'][confirm_id]
-        res, msg = await self.update_data(server, srv_data)
+        res, msg = await self.update_data_server(server, srv_data)
         return res, msg
 
     async def kolaborasi_putuskan(self, server, anime):
@@ -329,13 +384,13 @@ class naoTimesDB:
                 del osrvd['anime'][anime]['kolaborasi']
             else:
                 osrvd['anime'][anime]['kolaborasi'] = klosrv
-            res, msg = await self.update_data(osrv, osrvd)
+            res, msg = await self.update_data_server(osrv, osrvd)
             print('[ntDB] INFO: Acknowledged? {}'.format(res))
             print('[ntDB] Message: {}'.format(msg))
 
         del srv_data['anime'][anime]['kolaborasi']
 
-        res, msg = await self.update_data(server, srv_data)
+        res, msg = await self.update_data_server(server, srv_data)
         return res, msg
 
 
